eccac3c98222a23d38fb1668cbd01b260400c83e
nabiladigo <nabiladigo91@gmail.com>
2022-03-07 12:24:04 -0500

    update project page

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("/resume", (req,res) =>{
 });
 
 //declare a variable for our port number
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // turn on the server listener
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
